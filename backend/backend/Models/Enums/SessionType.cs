470500aea2ea904454c06523636cba41e3ba42cd
Edvin Becic <edvinbecic16@outlook.com>
2025-09-24 21:50:53 +0200

    add: initial models

---
+++ backend/backend/Models/Enums/SessionType.cs
@@ -0,0 +1,8 @@
+namespace backend.Models.Enums;
+
+public enum SessionType
+{
+    Lecture = 1,
+    Exercise = 2,
+    Other = 3
+}
---
+++ backend/backend/Models/Room.cs
@@ -0,0 +1,8 @@
+namespace backend.Models;
+
+public class Room
+{
+    public int Id { get; set; }
+    public string Building { get; set; }
+    public string Code { get; set; }
+}
