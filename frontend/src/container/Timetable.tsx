6d4f1e7de942433aba81c267beeb2cdfc78261ce
Edvin Becic <edvinbecic16@outlook.com>
2025-10-01 01:02:28 +0200

    fix: filter by day

--- frontend/src/container/Timetable.tsx
+++ frontend/src/container/Timetable.tsx
@@ -259,6 +259,7 @@ export function Timetable({
             calendarYear,
             month,
             day,
+            courseId,
             controller.signal
           );
           setEvents(data);
